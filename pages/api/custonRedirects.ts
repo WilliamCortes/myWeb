3208cd570ddcbe296e4e6d9e8c93854da90225da
WilliamCortesT <wcortes@talenta.cloud>
2023-05-12 12:52:57 -0500

    custom redirects

--- pages/api/custonRedirects.ts
+++ pages/api/custonRedirects.ts
@@ -1,12 +1,20 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
-const customRedirects: NextApiHandler = (req: NextApiRequest, res: NextApiResponse) => {
+const customRedirects: NextApiHandler = (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
   return res.status(200).json([
     {
       source: "/empleos/algo",
       destination: "/empresas/grupo-exito",
-      permanent: true
-    }
+      permanent: true,
+    },
+    {
+      source: "/empleos/redirect",
+      destination: "/trabajos/empleos-por-ciudades",
+      permanent: true,
+    },
   ]);
 };
 
