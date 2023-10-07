66b03e2b10305bae872261ad5d936e07bbfe10f0
WilliamCortesT <wcortes@talenta.cloud>
2023-10-06 23:16:21 -0500

    refactor: add gtm-id inline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,12 @@ import Script from "next/script";
 import "@/styles/globals.scss";
 import { Fragment } from "react";
 
-const gtmID = process.env.NEXT_PUBLIC_ANALYTICS_ID;
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Script
         async
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtmID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=G-V69WVT4EBQ`}
       />
       <Script id="google-analytics">
         {`
@@ -18,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', '${gtmID}');
+          gtag('config', 'G-V69WVT4EBQ');
         `}
       </Script>
       <Component {...pageProps} />
